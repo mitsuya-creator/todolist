a8561704c4c35560c5fe85f3e6c5d23a9b5bc8e5
@code <jj4068851@gmail.com>
2023-11-08 08:56:47 +0700

    Add: add more vertical icon

--- src/components/listTodosByActions.jsx
+++ src/components/listTodosByActions.jsx
@@ -1,32 +1,22 @@
 import React from "react";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 export default function ListTodosByActions() {
+    const testLoop = [0, 1, 2, 3]
     return (
         <ul className="container_list_by_actions">
-            <li className="body_list_by_actions">
-                <h1>React JS</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel officiis maiores modi odio animi commodi est reiciendis? Aliquam nobis praesentium fuga id ex eaque, architecto error ipsam labore necessitatibus odio.</p>
+            {testLoop.map((index) => <li className="body_list_by_actions" key={index}>
+                <section>
+                    <div>
+                        <h1>React JS</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel officiis maiores modi odio animi commodi est reiciendis? Aliquam nobis praesentium fuga id ex eaque, architecto error ipsam labore necessitatibus odio.</p>
+                    </div>
+                    <div className="more_vertical_icon">
+                        <MoreVertIcon onClick={() => console.log(index)} />
+                    </div>
+                </section>
             </li>
-            <li className="body_list_by_actions">
-                <h1>React Components</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel officiis maiores modi odio animi commodi est reiciendis? Aliquam nobis praesentium fuga id ex eaque, architecto error ipsam labore necessitatibus odio.</p>
-            </li>
-            <li className="body_list_by_actions">
-                <h1>React Components</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel officiis maiores modi odio animi commodi est reiciendis? Aliquam nobis praesentium fuga id ex eaque, architecto error ipsam labore necessitatibus odio.</p>
-            </li>
-            <li className="body_list_by_actions">
-                <h1>React Components</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel officiis maiores modi odio animi commodi est reiciendis? Aliquam nobis praesentium fuga id ex eaque, architecto error ipsam labore necessitatibus odio.</p>
-            </li>
-            <li className="body_list_by_actions">
-                <h1>React Components</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel officiis maiores modi odio animi commodi est reiciendis? Aliquam nobis praesentium fuga id ex eaque, architecto error ipsam labore necessitatibus odio.</p>
-            </li>
-            <li className="body_list_by_actions">
-                <h1>React Components</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel officiis maiores modi odio animi commodi est reiciendis? Aliquam nobis praesentium fuga id ex eaque, architecto error ipsam labore necessitatibus odio.</p>
-            </li>
+            )}
         </ul>
     )
 }
