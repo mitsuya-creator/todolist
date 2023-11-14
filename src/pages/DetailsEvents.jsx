192a7d31049dfcebae21c6cf05ea45065843476c
@code <jj4068851@gmail.com>
2023-11-15 06:33:26 +0700

    Add: adding style back button DetailEvents

--- src/pages/DetailsEvents.jsx
+++ src/pages/DetailsEvents.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import ButtonBackNavigation from "@/components/button/ButtonNavigationBack";
+import Slide from '@mui/material/Slide';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+
 export default function DetailEvents() {
     const { id } = useParams();
-    console.log(id)
     return (
-        <div>Details todo:  {id}</div>
+        <Slide direction="left" in={true} mountOnEnter unmountOnExit>
+            <div className="container_detail_events">
+                <div className="container_button_back_detail">
+                    <section className="button_back_title">
+                        <ButtonBackNavigation />
+                        <h1>Detail events</h1>
+                    </section>
+                    <section className="container_icon_title">
+                        <div>
+                            <EditIcon />
+                            <span>Edit</span>
+                        </div>
+                        <div>
+                            <DeleteIcon />
+                            <span>Delete</span>
+                        </div>
+                    </section>
+                </div>
+
+            </div>
+        </Slide>
     )
 }
