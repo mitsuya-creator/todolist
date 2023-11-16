a9149be54e70f8d72a77b7f946f1bab2f950200f
@code <jj4068851@gmail.com>
2023-11-17 06:47:47 +0700

    Add: add dependencies[x-date-picker,dayjs] for calendar component

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import * as React from 'react';
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { createTheme } from '@mui/material/styles'
+import { ThemeProvider } from '@emotion/react';
 
+const newTheme = (theme) => createTheme({
+    ...theme,
+    components: {
+        MuiPickersDay: {
+            styleOverrides: {
+                root: {
+                    color: '#bbdefb',
+                    borderRadius: 8,
+                    borderWidth: 0,
+                    borderColor: '#2196f3',
+                    border: '0px solid',
+                    backgroundColor: '#594AB2',
+                }
+            }
+        },
+        MuiDayCalendar: {
+            styleOverrides: {
+                weekDayLabel: {
+                    color: '#bbdefb',
+                    borderRadius: 0,
+                    borderWidth: 0,
+                    borderColor: '#e91e63',
+                    border: '0px solid',
+                    backgroundColor: '#594AB2',
+                }
+            }
+        }
+    }
+})
 export default function Calendar() {
+    const [value, setValue] = React.useState(dayjs('2023-04-17'));
+    console.log(value)
+
     return (
-        <div className="container_calendar">
-            <h1>Calendar</h1>
-        </div>
-    )
+        <ThemeProvider theme={newTheme}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DateCalendar', 'DateCalendar']}>
+                    <DemoItem label="Controlled calendar">
+                        <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
+                    </DemoItem>
+                </DemoContainer>
+            </LocalizationProvider>
+        </ThemeProvider>
+    );
 }
