26dda569937053dbd815a8dc53188babf07bfcb4
@code <jj4068851@gmail.com>
2023-11-21 06:50:14 +0700

    Reformat: reformatted style when completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,19 @@
-import { React } from "react";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import MoreButtonActions from "@/components/moreButtonActions";
-import { toggleMoreButtonActiveList } from "@/utils/toggleMoreButtonActiveList";
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import React from "react";
 import { Link } from "react-router-dom";
 
 function Card({ title, descriptions, id, isCompleted }) {
-    const handleMoreIcon = idCard => {
-        toggleMoreButtonActiveList(idCard);
-    }
     return (
-        <Link to={`/events/detail/${id}`}>
-            <li className="body_list_by_actions">
-                <section>
-                    <div>
-                        <h1>{title}{isCompleted ? <CheckCircleIcon fontSize={"small"} className="success_svg" /> : null} </h1>
+        <li className="body_list_by_actions">
+            <section>
+                <Link to={`/events/detail/${id}`} className="text_decoration_none">
+                    <div className={isCompleted && "line_through"}>
+                        <h1>{title}</h1>
                         <p>{descriptions}</p>
                     </div>
-                    <MoreVertIcon onClick={() => handleMoreIcon(id)} className="more_vertical_icon" />
-                    <MoreButtonActions id={id} />
-                </section>
-            </ li>
-        </Link>
+                </Link>
+                <input type="checkbox" checked={isCompleted} className={isCompleted ? "checkbox_card checked" : "checkbox_card"} />
+            </section>
+        </ li>
     )
 }
 export default Card;
